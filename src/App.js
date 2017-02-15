344794b5fd07ff8be9e88aeb1447277100e21a49
tswayne <swaynetyler@gmail.com>
2017-02-15 14:46:01 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import {games} from './video-game-titles';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+          <h2>React workshop 2</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
