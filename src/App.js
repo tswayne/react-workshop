096b63fddd52e0e2e31c2957648697c766f007a0
tswayne <swaynetyler@gmail.com>
2017-02-16 16:05:31 -0800

    solution

--- src/App.js
+++ src/App.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 import {games} from './video-game-titles';
 
+class Results extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.results.map((result, index) => {
+          return (<li key={index}>{result}</li>)
+        })}
+      </ul>
+    )
+  }
+}
+
+class Input extends Component {
+  render() {
+    return( <input onChangeEvent={this.props.onChange} type="text"/>)
+  }
+}
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.state = {results: []}
   }
 
+  onUpdate = (e) => {
+    const matches = games.filter(game => {
+      return game.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+    this.setState({results: matches})
+  }
 
   render() {
     return (
       <div className="App">
-          <h2>React workshop 2</h2>
+        <h2>React workshop 2</h2>
+        <Input onChangeEvent={this.onUpdate} type="text"/>
+        <Results results={this.state.results}/>
       </div>
     );
   }
